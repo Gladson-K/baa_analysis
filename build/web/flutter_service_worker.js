44ea0e61ed0b787760cf93349218f7a1d421b8dd
Gladson-K <93399604+Gladson-K@users.noreply.github.com>
2024-11-17 19:25:55 +0530

    Re-add Flutter web app files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "63689aae686839e7c7dd6dd94c9ad23c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "387d5680d0b8250c54e1f85bbd5fd7da",
+"flutter_bootstrap.js": "6c58b741cc14b3f1c778feac6faa078b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
